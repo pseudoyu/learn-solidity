cce981118fbae156d10448c9fcb682ecbc054b2b
Yu Zhang <pseudoyu@connect.hku.hk>
2022-06-16 04:27:32 +0000

    feat: add Chainlink Keeper implementation

--- hardhat/hardhat_lottery/contracts/Lottery.sol
+++ hardhat/hardhat_lottery/contracts/Lottery.sol
@@ -4,11 +4,14 @@ pragma solidity 0.8.7;
 // 引用
 import "@chainlink/contracts/src/v0.8/VRFConsumerBaseV2.sol";
 import "@chainlink/contracts/src/v0.8/interfaces/VRFCoordinatorV2Interface.sol";
+import "@chainlink/contracts/src/v0.8/interfaces/KeeperCompatibleInterface.sol";
 
 // 错误码
 error Lottery__NotEnoughEntranceFee();
 error Lottery__NotOwner();
 error Lottery__SendFailed();
+error Lottery__NotOpen();
+error Lottery__UpkeepNotNeeded(uint256 currentBalance, uint256 numPlayers, uint256 lotteryState);
 
 /**
  * @title Lottery Smart Contract
@@ -17,7 +20,13 @@ error Lottery__SendFailed();
  * @dev 2. Pick random winner - Chainlink Oracle
  * @dev 3. Automatically run - Chainlink Keeper
  */
-contract Lottery is VRFConsumerBaseV2 {
+contract Lottery is VRFConsumerBaseV2, KeeperCompatibleInterface {
+    // 类型定义
+    enum LotteryState {
+        OPEN,
+        CALCULATING
+    }
+
     // 状态变量
     address private immutable i_owner;
     uint256 private immutable i_entranceFee;
@@ -31,6 +40,9 @@ contract Lottery is VRFConsumerBaseV2 {
 
     // Lottery 变量
     address private s_recentWinner;
+    LotteryState private s_lotteryState;
+    uint256 private s_lastTimeStamp;
+    uint256 private immutable i_interval;
 
     // 事件
     event LotteryEnter(address indexed player);
@@ -51,7 +63,8 @@ contract Lottery is VRFConsumerBaseV2 {
         uint256 entranceFee,
         bytes32 keyHash,
         uint64 subscriptionId,
-        uint32 callbackGasLimit
+        uint32 callbackGasLimit,
+        uint256 interval
     ) VRFConsumerBaseV2(vrfCoordinatorV2) {
         i_owner = msg.sender;
         i_entranceFee = entranceFee;
@@ -59,15 +72,31 @@ contract Lottery is VRFConsumerBaseV2 {
         i_keyHash = keyHash;
         i_subscriptionId = subscriptionId;
         i_callbackGasLimit = callbackGasLimit;
+        s_lotteryState = LotteryState.OPEN;
+        s_lastTimeStamp = block.timestamp;
+        i_interval = interval;
     }
 
     // external 函数
+
     /**
      * @notice For requesting the random number
-     * @dev 1. Request the random number
-     * @dev 2. Do something with the random number
+     * @dev Use Chainlink VRF to get random number
+     * 1. Request the random number
+     * 2. Do something with the random number
      */
-    function requestRandomWinner() external {
+    function performUpkeep(
+        bytes calldata /* performData */
+    ) external override {
+        (bool upkeepNeeded, ) = checkUpkeep("");
+        if (!upkeepNeeded) {
+            revert Lottery__UpkeepNotNeeded(
+                address(this).balance,
+                s_players.length,
+                uint256(s_lotteryState)
+            );
+        }
+        s_lotteryState = LotteryState.CALCULATING;
         uint256 requestId = i_vrfCoordinator.requestRandomWords(
             i_keyHash,
             i_subscriptionId,
@@ -83,8 +112,37 @@ contract Lottery is VRFConsumerBaseV2 {
         if (msg.value < i_entranceFee) {
             revert Lottery__NotEnoughEntranceFee();
         }
+        if (s_lotteryState != LotteryState.OPEN) {
+            revert Lottery__NotOpen();
+        }
         s_players.push(payable(msg.sender));
         emit LotteryEnter(msg.sender);
+    }
+
+    /**
+     * @notice Chainlink Keeper Check
+     * @dev Check Upkeep
+     * 1. Pass the time interval
+     * 2. Should at least 1 player with ETH
+     * 3. Subscripotion should be funded with LINK
+     * 4. The lottery should in "open" state
+     */
+    function checkUpkeep(
+        bytes memory /* checkData */
+    )
+        public
+        view
+        override
+        returns (
+            bool upkeepNeeded,
+            bytes memory /* performData */
+        )
+    {
+        bool isOpen = (LotteryState.OPEN == s_lotteryState);
+        bool timePassed = ((block.timestamp - s_lastTimeStamp) > i_interval);
+        bool hasPlayers = (s_players.length > 0);
+        bool hasBalance = (address(this).balance > 0);
+        upkeepNeeded = (isOpen && timePassed && hasPlayers && hasBalance);
     }
 
     // internal 函数
@@ -98,6 +156,9 @@ contract Lottery is VRFConsumerBaseV2 {
         uint256 indexOfWinner = randomWords[0] % s_players.length;
         address payable recentWinner = s_players[indexOfWinner];
         s_recentWinner = recentWinner;
+        s_lotteryState = LotteryState.OPEN;
+        s_players = new address payable[](0);
+        s_lastTimeStamp = block.timestamp;
         (bool sendSuccess, ) = recentWinner.call{value: address(this).balance}("");
         if (!sendSuccess) {
             revert Lottery__SendFailed();
