d922765bb7e7adf65284c4820cebefd919fce7f3
Yu Zhang <pseudoyu@connect.hku.hk>
2022-06-17 02:55:47 +0000

    feat: add web3modal execute

--- nextjs/nextjs_web3modal/pages/index.js
+++ nextjs/nextjs_web3modal/pages/index.js
@@ -3,6 +3,9 @@ import Image from "next/image";
 import styles from "../styles/Home.module.css";
 import Web3Modal from "web3modal";
 import WalletConnectProvider from "@walletconnect/web3-provider";
+import { useState, useEffect } from "react";
+import { ethers } from "ethers";
+import { abi } from "../constants/abi";
 
 let web3Modal;
 
@@ -16,17 +19,69 @@ const providerOptions = {
 };
 
 export default function Home() {
+	const [isConnected, setIsConnected] = useState(false);
+	const [hasMetamask, setHasMetamask] = useState(false);
+	const [signer, setSigner] = useState(undefined);
+
+	useEffect(() => {
+		if (typeof window.ethereum !== "undefined") {
+			setHasMetamask(true);
+		}
+	});
+
 	async function connect() {
-		web3Modal = new Web3Modal({
-			cacheProvider: false,
-			providerOptions: providerOptions,
-		});
-		const web3ModalProvider = await web3Modal.connect();
-		const provider = new ethers.providers.Web3Provider(web3ModalProvider);
+		if (typeof window.ethereum !== "undefined") {
+			try {
+				web3Modal = new Web3Modal({
+					cacheProvider: false,
+					providerOptions: providerOptions,
+				});
+				const web3ModalProvider = await web3Modal.connect();
+				setIsConnected(true);
+				const provider = new ethers.providers.Web3Provider(
+					web3ModalProvider
+				);
+				setSigner(provider.getSigner());
+			} catch (e) {
+				console.log(e);
+			}
+		} else {
+			setIsConnected(false);
+		}
 	}
+
+	async function execute() {
+		if (typeof window.ethereum !== "undefined") {
+			const contractAddress =
+				"0x5FbDB2315678afecb367f032d93F642f64180aa3";
+			const contract = new ethers.Contract(contractAddress, abi, signer);
+			try {
+				await contract.store(42);
+			} catch (error) {
+				console.log(error);
+			}
+		} else {
+			console.log("Please install MetaMask");
+		}
+	}
+
 	return (
-		<div className={styles.container}>
-			<button onClick={() => connect()}>Connect</button>
+		<div>
+			{hasMetamask ? (
+				isConnected ? (
+					"Connected! "
+				) : (
+					<button onClick={() => connect()}>Connect</button>
+				)
+			) : (
+				"Please install metamask"
+			)}
+
+			{isConnected ? (
+				<button onClick={() => execute()}>Execute</button>
+			) : (
+				""
+			)}
 		</div>
 	);
 }
