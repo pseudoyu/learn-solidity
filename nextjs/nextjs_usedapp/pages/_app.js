73f0ecdf3c351be9a28b67663571e4508395c943
Yu Zhang <pseudoyu@connect.hku.hk>
2022-06-17 03:08:43 +0000

    feat: add nextjs-usedapp

---
+++ nextjs/nextjs_usedapp/pages/_app.js
@@ -0,0 +1,16 @@
+import "../styles/globals.css";
+import { DAppProvider } from "@usedapp/core";
+
+const config = {
+	multicallAddresses: ["0x5FbDB2315678afecb367f032d93F642f64180aa3"],
+};
+
+function MyApp({ Component, pageProps }) {
+	return (
+		<DAppProvider config={config}>
+			<Component {...pageProps} />
+		</DAppProvider>
+	);
+}
+
+export default MyApp;
---
+++ nextjs/nextjs_usedapp/pages/index.js
@@ -0,0 +1,48 @@
+import Head from "next/head";
+import Image from "next/image";
+import styles from "../styles/Home.module.css";
+import { useEthers, useContractFunction } from "@usedapp/core";
+import { useState, useEffect } from "react";
+import { ethers } from "ethers";
+import { abi } from "../constants/abi";
+
+export default function Home() {
+	const { activateBrowserWallet, account } = useEthers();
+	const [hasMetamask, setHasMetamask] = useState(false);
+
+	useEffect(() => {
+		if (typeof window.ethereum !== "undefined") {
+			setHasMetamask(true);
+		}
+	});
+
+	async function connect() {
+		await activateBrowserWallet();
+	}
+	const contractAddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
+	const contract = new ethers.Contract(contractAddress, abi);
+
+	const { send, state } = useContractFunction(contract, "store", {
+		transactionName: "store",
+	});
+
+	useEffect(() => {
+		console.log(`State: ${state.status}`);
+	}, [state]);
+
+	return (
+		<div>
+			{hasMetamask ? (
+				account ? (
+					"Connected! "
+				) : (
+					<button onClick={() => connect()}>Connect</button>
+				)
+			) : (
+				"Please install metamask"
+			)}
+
+			{account ? <button onClick={() => send(42)}>Execute</button> : ""}
+		</div>
+	);
+}
