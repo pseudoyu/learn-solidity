c0bca3a1bb91df0e8914c517b917b36d356f8188
pseudoyu <pseudoyu@connect.hku.hk>
2022-08-16 14:13:32 +0800

    feat: use _setTokenURI function from ERC721/extensions/ERC721URIStorage.sol

--- hardhat/hardhat_nft/contracts/BasicNft.sol
+++ hardhat/hardhat_nft/contracts/BasicNft.sol
@@ -2,26 +2,29 @@
 
 pragma solidity ^0.8.7;
 
-import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+// import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 
-contract BasicNft is ERC721 {
+contract BasicNft is ERC721URIStorage {
     string public constant TOKEN_URI =
         "ipfs://bafybeig37ioir76s7mg5oobetncojcm3c3hxasyd4rvid4jqhy4gkaheg4/?filename=0-PUG.json";
     uint256 private s_tokenCounter;
+
+    // Events
+    event NftMinted(uint256 indexed tokenId, string indexed tokenUri, address indexed minter);
 
     constructor() ERC721("DemoNft", "DNFT") {
         s_tokenCounter = 0;
     }
 
     function mintNft() public returns (uint256) {
+        uint256 newTokenId = s_tokenCounter;
         _safeMint(msg.sender, s_tokenCounter);
+        _setTokenURI(newTokenId, TOKEN_URI);
+        emit NftMinted(newTokenId, TOKEN_URI, msg.sender);
         s_tokenCounter = s_tokenCounter + 1;
         return s_tokenCounter; 
-    }
-
-    function tokenURI(uint256 tokenId) public view override returns (string memory) {
-        return TOKEN_URI;
-    }
+    } 
 
     function getTokenCounter() public view returns (uint256) {
         return s_tokenCounter;
