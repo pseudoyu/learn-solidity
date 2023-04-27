ee0e6250c0fa7ae060abf726f62cb49c4aa3912f
pseudoyu <pseudoyu@connect.hku.hk>
2023-04-27 13:52:22 +0800

    feat: add remapping.txt config

--- foundry/hello-foundry/src/Imports.sol
+++ foundry/hello-foundry/src/Imports.sol
@@ -1,2 +1,9 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.16;
+
+import "solmate/tokens/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract Token is ERC20("name", "symbol", 18) {}
+
+contract TestOZ is Ownable {}
